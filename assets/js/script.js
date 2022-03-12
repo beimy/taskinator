992e159b729119d668b23298a461b1f4dcb3ef92
Beimy <arbm159357@gmail.com>
2022-03-11 20:15:05 -0500

    improved UI/UX by adding submission listeners and removing default page behaviors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl= document.querySelector("#tasks-to-do");
 
-var creatTaskHandler = function(){
+var creatTaskHandler = function(event){
+    event.preventDefault();
+
+
     var listItemEl = document.createElement("li");
     listItemEl.textContent = "This is a new task.";
     listItemEl.className = "task-item";
     tasksToDoEl.append(listItemEl);
-}
+};
 
-buttonEl.addEventListener("click", creatTaskHandler);
+formEl.addEventListener("submit", creatTaskHandler);
 
